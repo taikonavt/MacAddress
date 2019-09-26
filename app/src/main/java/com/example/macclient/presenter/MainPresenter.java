44e3ebcd4addc45d97dd51ec56925f95895d116b
taikonavt <taikonavt@gmail.com>
2019-09-26 23:26:02 +0300

    make interface

---
+++ app/src/main/java/com/example/macclient/presenter/MainPresenter.java
@@ -0,0 +1,15 @@
+package com.example.macclient.presenter;
+
+import com.example.macclient.view.MainView;
+
+public class MainPresenter {
+    private MainView mainView;
+
+    public MainPresenter(MainView mainView) {
+        this.mainView = mainView;
+    }
+
+    public void onRequestBtnClick() {
+
+    }
+}
---
+++ app/src/main/java/com/example/macclient/view/MainView.java
@@ -0,0 +1,4 @@
+package com.example.macclient.view;
+
+public interface MainView {
+}
